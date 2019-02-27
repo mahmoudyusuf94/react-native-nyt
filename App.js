e91ce0890f303207933e0f81c8f27130a04f6106
Mahmoud Yusuf <mahmoud.yusuf@blink22.com>
2019-02-27 13:52:52 +0200

    Simple react-navigation implementation

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import HomeScreen from './src/components/HomeScreen';
+import React, {Component} from 'react';
+import {createStackNavigator, createAppContainer} from 'react-navigation'
+import IntroScreen from './src/components/IntroScreen';
+import { Provider } from 'react-redux';
 import createStore from './src/createStore';
+import HomeScreen from './src/components/HomeScreen';
 
+const AppNavigator = createStackNavigator(
+    {
+    Intro: IntroScreen,
+    Feed: HomeScreen
+    }, {
+        initialRouteName: 'Intro'
+    }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
 const store = createStore();
 
-export default () => (
-  <Provider store = {store}>
-    <HomeScreen />
-  </Provider>
-)
+export default class App extends Component {
+    render() {
+        return (
+        <Provider store={store}>
+            <AppContainer />
+        </Provider>
+        ); 
+    }
+}
---
+++ src/components/IntroScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import Title from './Title';
+import AppText from './AppText';
+import * as globalStyles from '../styles/global';
+
+const IntroScreen = (props) => (
+    <View style={[globalStyles.COMMON_STYLES.pageContainer, styles.container]}>
+        <TouchableOpacity onPress ={ () =>  { props.navigation.navigate('Feed')}}>
+            <Title>React Native News Reader</Title>
+            <AppText>Start Reading</AppText>
+        </TouchableOpacity>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default IntroScreen;
